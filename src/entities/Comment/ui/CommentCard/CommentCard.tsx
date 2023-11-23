e7373301c4fc5d3ed6477e3cd7009cc9cbc5eb41
Igor Delendik <knoxville89@mail.ru>
2023-11-23 19:11:32 +0300

    Fix some VStacks

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -19,13 +19,13 @@ interface CommentCardProps {
 export const CommentCard = memo(({ className, comment, isLoading }: CommentCardProps) => {
     if (isLoading) {
         return (
-            <div className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
+            <VStack gap="8" max className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
                 <div className={cls.header}>
                     <Skeleton width={30} height={30} border="50%" className={cls.header} />
                     <Skeleton width={100} height={16} className={cls.username} />
                 </div>
                 <Skeleton width="100%" height={50} className={cls.text} />
-            </div>
+            </VStack>
         );
     }
 
