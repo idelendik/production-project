20fd34a40b45446f9bb43da09fe6e903815416d8
Igor Delendik <knoxville89@mail.ru>
2024-02-05 16:20:39 +0300

    Fix Drawer. Add .browserslistrc

--- src/features/notificationButton/ui/notificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/notificationButton/NotificationButton.tsx
@@ -9,7 +9,6 @@ import { Popover } from "@/shared/ui/Popups";
 import { Drawer } from "@/shared/ui/Drawer/Drawer";
 
 import { BrowserView, MobileView } from "react-device-detect";
-import { AnimationProvider } from "@/shared/lib/components/AnimationProvider";
 
 interface NotificationButtonProps {
     className?: string
@@ -45,11 +44,9 @@ export const NotificationButton = memo(({ className }: NotificationButtonProps) 
 
             <MobileView>
                 {trigger}
-                <AnimationProvider>
-                    <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                        <NotificationList />
-                    </Drawer>
-                </AnimationProvider>
+                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                    <NotificationList />
+                </Drawer>
             </MobileView>
         </div>
     );
--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -6,6 +6,7 @@ import cls from "./Drawer.module.scss";
 import { Portal } from "../Portal/Portal";
 import { Overlay } from "../Overlay/Overlay"
 
+import { AnimationProvider } from "@/shared/lib/components/AnimationProvider";
 
 import { useAnimationLibs } from "@/shared/lib/components/AnimationProvider";
 
@@ -91,14 +92,21 @@ const DrawerContent = memo((props: DrawerProps) => {
     )
 })
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = (props: DrawerProps) => {
     const { isLoaded } = useAnimationLibs();
 
     if (!isLoaded) return null;
 
     return <DrawerContent {...props} />
-});
+};
+
+export const Drawer = (props: DrawerProps) => {
+    return (
+        <AnimationProvider>
+            <DrawerAsync {...props} />
+        </AnimationProvider>
+    )
+};
 
 // Fix for memo - ESLint: Component definition is missing display name(react/display-name)
-Drawer.displayName = "Drawer"
 DrawerContent.displayName = "DrawerContent"
