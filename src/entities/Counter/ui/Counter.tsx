6bce8a733aa765cc677247c0a3ab355b36a129d7
Igor Delendik <knoxville89@mail.ru>
2024-02-16 14:46:49 +0300

    Add helpers for slices, selectors, actions.

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/shared/ui/Button";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { counterActions } from "../model/slice/counterSlice";
-import { getCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
+import { useCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
 
 export const Counter = () => {
     const dispatch = useDispatch();
-    const counterValue = useSelector(getCounterValue);
+    const counterValue = useCounterValue();
 
     const decrement = () => { dispatch(counterActions.decrement()) };
     const increment = () => { dispatch(counterActions.increment()) };
