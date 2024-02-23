b19d255e254d12566d4d37b1a0fa88be422faf90
Igor Delendik <knoxville89@mail.ru>
2024-02-23 17:04:51 +0300

    Add argument to buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,12 +1,19 @@
 import { useSelector } from 'react-redux';
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+type Selector<T, Args extends any[]> = (state: StateSchema, ...args: Args) => T;
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-    const useSelectorHook = () => {
-        return useSelector(selector);
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+export function buildSelector<T, Args extends any[]>(
+    selector: Selector<T, Args>,
+): Result<T, Args> {
+    const useSelectorHook: Hook<T, Args> = (...args: Args) => {
+        return useSelector((state: StateSchema) => selector(state, ...args));
     };
 
     return [useSelectorHook, selector];
