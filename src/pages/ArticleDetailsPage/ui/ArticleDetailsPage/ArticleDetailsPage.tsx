e01b7b6d140b80eb5e34463f39927c48d0b301dd
Igor Delendik <knoxville89@mail.ru>
2024-02-27 17:08:50 +0300

    Add toggle features for components.

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -15,7 +15,7 @@ import { VStack } from '@/shared/ui/Stack';
 import { ArticleRecommendationsList } from '@/features/articleRecommendationsList';
 import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
 import { ArticleRating } from '@/features/articleRating';
-import { toggleFeatures } from '@/shared/lib/features';
+import { ToggleFeatures } from '@/shared/lib/features';
 import { Card } from '@/shared/ui/Card';
 import { useTranslation } from 'react-i18next';
 
@@ -35,12 +35,6 @@ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
         return null;
     }
 
-    const articleRatingCard = toggleFeatures({
-        name: 'isArticleRatingEnabled',
-        on: () => <ArticleRating articleId={id} />,
-        off: () => <Card>{t('article_rating_will_be_available_soon')}</Card>,
-    });
-
     return (
         <DynamicModuleLoader reducers={reducers}>
             <Page
@@ -51,7 +45,15 @@ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
 
                     <ArticleDetails id={id} />
 
-                    {articleRatingCard}
+                    <ToggleFeatures
+                        feature={'isArticleRatingEnabled'}
+                        on={<ArticleRating articleId={id} />}
+                        off={
+                            <Card>
+                                {t('article_rating_will_be_available_soon')}
+                            </Card>
+                        }
+                    />
 
                     <ArticleRecommendationsList />
 
