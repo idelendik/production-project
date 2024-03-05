70f60685b733089e782bc1df406ce01a9349e6e1
Igor Delendik <knoxville89@mail.ru>
2024-03-05 20:51:06 +0300

    Add force update.

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -25,6 +25,7 @@ import { Button } from '@/shared/ui/redesigned/Button';
 import { Input } from '@/shared/ui/redesigned/Input';
 import { Text } from '@/shared/ui/redesigned/Text';
 import { VStack } from '@/shared/ui/redesigned/Stack';
+import { useForceUpdate } from '@/shared/lib/render/forceUpdate';
 
 export interface LoginFormProps {
     className?: string;
@@ -44,6 +45,8 @@ const LoginForm = memo(({ className, onSuccess }: LoginFormProps) => {
     const password = useSelector(getLoginPassword);
     const isLoading = useSelector(getLoginIsLoading);
     const error = useSelector(getLoginError);
+
+    const forceUpdate = useForceUpdate();
 
     useEffect(() => {
         return () => {
@@ -69,8 +72,9 @@ const LoginForm = memo(({ className, onSuccess }: LoginFormProps) => {
         const result = await dispatch(loginByUsername({ username, password }));
         if (result.meta.requestStatus === 'fulfilled') {
             onSuccess();
+            forceUpdate();
         }
-    }, [onSuccess, dispatch, username, password]);
+    }, [onSuccess, dispatch, username, password, forceUpdate]);
 
     return (
         <DynamicModuleLoader reducers={initialReducers}>
